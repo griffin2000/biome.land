6a614b4969f219a34a1ee33fd8d9ed8714de7c98
Neal Manaktola <nealmanaktola@gmail.com>
2019-12-19 12:09:34 -0800

    Add camera controls and update initial position

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'; 
 import {GLTFLoader} from './third-party/GLTFLoader'
+
+var controls; 
+var camera;
 
 async function init() {
   const canvas = document.getElementById('canvas');
@@ -7,19 +11,17 @@ async function init() {
     antialias: true,
     canvas 
   } );
-  const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 80000 );
+  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 80000 );
 
-  camera.position.y = 4;
-  camera.lookAt(new THREE.Vector3(0,0,-10));
+  camera.position.set(-7, 0, -21);
+  controls = new OrbitControls( camera, renderer.domElement );
 
   const scene = new THREE.Scene();
   scene.background = new THREE.Color( 0xAAAAAA );
 
-
   const light = new THREE.DirectionalLight( 0xFFFFFF, 3.0 );
   scene.add(light);
   light.target.position.y = -10;
-
   
   var loader = new THREE.GLTFLoader();
   loader.load( 'assets/map_139586444_densified_gltf/map_139586444_densified_mesh_textured_lod0z.gltf', function ( gltf ) {
@@ -38,6 +40,12 @@ async function init() {
   console.log("init");
 }
 
+function animate() {
+  requestAnimationFrame( animate );
+  controls.update();
+}
+
 init().then(()=>{
   console.log("App inited");
+  animate()
 });
